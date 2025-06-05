bb2776af7eb0dae73b89d72e2817829d01dec67a
Jakub Brož <brozikcz@gmail.com>
2025-06-05 16:20:09 +0200

    Test

--- custom_components/aladin_online/aladin_online.py
+++ custom_components/aladin_online/aladin_online.py
@@ -134,7 +134,7 @@ class AladinOnlineCoordinator(DataUpdateCoordinator):
 			raise ServiceUnavailable
 
 		data_time = await AladinOnlineCoordinator._format_datetime(self._data[DATA_TIME])
-		now = datetime.now() + timedelta(hours=1)
+		now = datetime.now()
 
 		actual_index = int(math.floor((now.timestamp() - data_time.timestamp()) / 3600))
 		condition_actual_index = int(math.floor(actual_index / 2))
